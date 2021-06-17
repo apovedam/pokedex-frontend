5a9848a27e3848fc6e060f392d6f17224730efc3
Adrian Poveda <apovedamckay@gmail.com>
2021-06-16 21:29:26 -0500

    implemented Pokedex screen

--- src/Components/Dex.js
+++ src/Components/Dex.js
@@ -1,7 +1,129 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  CardActions,
+  Container,
+  Grid,
+  Typography,
+  Button,
+  Paper,
+} from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 
-function Dex() {
-  return <div></div>;
-}
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    // paddingTop: "56.25%", // 16:9
+    paddingTop: "100%", // 16:9
+    backgroundColor: "#e0e0e0",
+    color: "#FFFFFF",
+    height: 0,
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+
+  root: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "row",
+  },
+  details: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  remove: { display: "flex", flexDirection: "column" },
+  cover: {
+    height: 150,
+    width: 150,
+  },
+  root2: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: "auto",
+    maxWidth: 500,
+  },
+  image: {
+    width: 128,
+    height: 128,
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
+  },
+}));
+
+const Dex = (props) => {
+  const classes = useStyles();
+
+  const history = useHistory();
+  const handleOnClick = (a) => history.push("/Detail/" + a);
+
+  return (
+    <Container className={classes.cardGrid} maxWidth="md">
+      <Grid container spacing={4}>
+        {props.dexPokes.map((card) => (
+          <Grid item key={card} xs={12} sm={6} md={4}>
+            <Card className={classes.card}>
+              <CardMedia
+                className={classes.cardMedia}
+                image={card.sprites.front_default}
+                title="Image title"
+              />
+              <CardContent className={classes.cardContent}>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {card.name}
+                </Typography>
+                <Typography>#{card.id}</Typography>
+              </CardContent>
+              <CardActions>
+                <Button
+                  size="small"
+                  color="primary"
+                  onClick={() => props.removeDexPoke(card.id)}
+                >
+                  Remove from Pokedex
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
+
+Dex.defaultProps = {
+  dexPokes: [],
+};
 
 export default Dex;
